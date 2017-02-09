c3b12b4707b5c78a486f61ae2b316f095ff9149c
BirchBark <cburich@wisc.edu>
2017-02-09 17:54:19 -0600

    Update GradeFileFormatException.java

--- GradeFileFormatException.java
+++ GradeFileFormatException.java
@@ -1,11 +1,20 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2016 
+// PROJECT:          p1
+// FILE:             GradeFileFormatException
+//
+// Author: Meggie Cook,  
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
 /**
+ * An exception that is checked and thrown when a file isn't in the specified
+ * format for the GradeEstimator
  * 
  * @author Meggie Cook
- *
  */
 public class GradeFileFormatException extends Exception{
 	/**
-	 * 
+	 * Constructs the exception
 	 */
 	public GradeFileFormatException(){
 		super();

8dad04fa24c80933ff10124eda338ccd0f121bdf
JFernandes2 <jfernandes2@wisc.edu>
2017-02-09 18:01:38 -0600

    Update ScoreIteratorADT.java

--- ScoreIteratorADT.java
+++ ScoreIteratorADT.java
@@ -1,20 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2016 
+// PROJECT:          program1
+// FILE:             ScoreIteratorADT.java
+//
+// Authors: (Team 42) Aseel Albeshri, Cory Burich, Margaret Cook, 
+//          Jessica Fernandes, Cody Kairis, Jacob Vande Walle
+// Author1: Aseel Albeshri, albeshri@wisc.edu, albeshri,lecture 002
+// Author2: Cory Burich, cburich@wisc.edu, cburich, lecture 002
+// Author3: Margaret Cook, mcook24@wisc.edu, mcook24, lecture 002
+// Author4: Jessica Fernandes, jfernandes2@wisc.edu, jfernandes2, lecture 002
+// Author5: Cody Kairis, kairis@wisc.edu, kairis, lecture 002
+// Author6: Jacob Vande Walle, jvandewalle@wisc.edu, jvandewalle, lecture 002
+//
 /**
  * 
- * @author Margaret Cook
+ * @authors Aseel Albeshri, Cory Burich, Margaret Cook, Jessica Fernandes, 
+ * Cody Kairis, Jacob Vande Walle
  *
  */
 public interface ScoreIteratorADT<E>{
 	
 	
 	/**
-	 * Indicates if there is another score 
-	 * @return true if there is another score 
+	 * Indicates if there is another score. Returns true if there is 
+	 * another score and false after the last score.
+	 *
+	 * PRECONDITIONS: (i.e. the incoming list is assumed to be non-null)
+	 * 
+	 * POSTCONDITIONS: (i.e. the incoming list has been reordered)
+	 * 
+	 * @return true if there is another score
 	 */
 	boolean hasNext();
 	
 	/**
+	 * Generic type, returns next input before a space. 
+	 *
+	 * PRECONDITIONS: (i.e. the incoming list is assumed to be non-null)
 	 * 
-	 * @return
+	 * POSTCONDITIONS: (i.e. the incoming list has been reordered)
+	 *
+	 * @return next input of a generic type or type specified
 	 */
 	E next();
 	

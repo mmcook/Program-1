02cfcf17c82001f8115bd8f44974138a8177b38d
Jake VandeWalle <jakevandewalle@gmail.com>
2017-02-10 14:42:13 -0600

    Added category recognization

--- Score.java
+++ Score.java
@@ -1,88 +1,87 @@
-/////////////////////////////////////////////////////////////////////////////
-// Semester:         CS367 Spring 2016 
-// PROJECT:          p0
-// FILE:             Score.java
-//
-// Authors: Margaret Cook, mcook24@wisc.edu, 9074490211, Lecture002
-//////////////////////////// 80 columns wide //////////////////////////////////
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+/////////////////////////////////////////////////////////////////////////////
+//Semester:         CS367 Spring 2016 
+//PROJECT:          p1
+//FILE:            ScoreIterator.java
+//
+//Authors: (Team 42) Aseel Albeshri, Cory Burich, Margaret Cook, 
+//Jessica Fernandes, Cody Kairis, Jacob Vande Walle
+//Author1: Aseel Albeshri, albeshri@wisc.edu, albeshri,lecture 002
+//Author2: Cory Burich, cburich@wisc.edu, cburich, lecture 002
+//Author3: Margaret Cook, mcook24@wisc.edu, mcook24, lecture 002
+//Author4: Jessica Fernandes, jfernandes2@wisc.edu, jfernandes2, lecture 002
+//Author5: Cody Kairis, kairis@wisc.edu, kairis, lecture 002
+//Author6: Jacob Vande Walle, jvandewalle@wisc.edu, jvandewalle, lecture 002
+//
 /**
- * This class controls each of the scores in the list. It contains the name
- * of the assignment, the points earned in the assignment and the maximum
- * points that is possible to get in the assignment. Additionally, it returns
- * the category of the assignment and the percentage earned.
- * @author Margaret Cook
+* 
+* @authors Aseel Albeshri, Cory Burich, Margaret Cook, Jessica Fernandes, 
+* Cody Kairis, Jacob Vande Walle
+/**
+ * This class acts as an indirect access iterator. 
+ * It implements the ScoreIteratorADT and iterates through the given scores in
+ *  ScoreList that match a specified category.
  *
  */
-public class Score {
-	//The assignment name
-	private String name;
-	
-	//The points earned in the assignment
-	private double pointsEarned;
-	
-	//The points possible for the assignment
-	private double pointsPossible;
-	
-	/**
-	 * The constructor for the scores, and assigns the name, points earned and total points possible
-	 * for this particular score
-	 * @param name is the assignment name
-	 * @param pointsEarned is the points earned
-	 * @param pointsPossible is the possible points for the assignment
-	 * @throws IllegalArgumentException
-	 */
-	public Score(String name, double pointsEarned, double pointsPossible) throws IllegalArgumentException{
-		//If the name is null, the points earned are negative, the total points possible are negative
-		//and/or the points earned are larger than the possible points, throw an exception
-		if(name == null|| pointsEarned < 0|| pointsPossible < 0|| pointsEarned > pointsPossible)
-			throw new IllegalArgumentException();
-		
-		this.name = name;
-		this.pointsEarned = pointsEarned;
-		this.pointsPossible = pointsPossible;
-	}
-	
-	/**
-	 * Gives the assignment name of the score
-	 * @return the assignment name
-	 */
-	public String getName(){
-		return name;
-	}
-	
-	/**
-	 * Returns the points that one has earned in the assignment
-	 * @return
-	 */
-	public double getPoints(){
-		return pointsEarned;
-	}
-	
-	/**
-	 * Returns the maximum points that one can earn in the assignment
-	 * @return
-	 */
-	public double getMaxPossible(){
-		return pointsPossible;
-	}
-	
-	/**
-	 * Returns the category of the assignment, I.E the first letter of the
-	 * assignment name
-	 * @return the first letter of name
-	 */
-	public String getCategory(){
-		return name.substring(0, 1);
-	}
-	
-	/**
-	 * Returns the percentage of the score, which is calculated by dividing the
-	 * points earned by the maximum point, and make it into a percentage
-	 * @return
-	 */
-	public double getPercent(){
-		return (pointsEarned / pointsPossible) * 100;
-	}
+public class ScoreIterator implements ScoreIteratorADT<Score>, Iterator<Score>{
+	//Variable of type ScoreList to store the list of scores
+	private ScoreList scorelist;
+	//Variable of type int to store the current position
+	private int currPos;
+	//Variable of type string to store a category
+	private String category;
+	// Stores the Score object that we return
+	private Score tempScore = null;
 
+	/**
+	 * Initializes the instance fields to the passed values 
+	 * and sets the current position to 0.
+	 * PRECONDITIONS: instance fields are null
+	 * POSTCONDITIONS: instance fields are initialized 
+	 * @param scorelist the given list of scores
+	 * @param category the given category
+	 */
+	public ScoreIterator(ScoreList scorelist, String category){
+		this.scorelist = scorelist;
+		this.category = category;
+		currPos = 0;
+	}
+	/**
+	 * Method used to check if scorelist has a next item
+	 * @return true if scoreList is bigger than current position, false otherwise
+	 */
+	public boolean hasNext(){
+		return currPos < scorelist.size();
+	}
+	/**
+	 * Method used to get the next item in scorelist 
+	 * @throws NoSuchElementException if scoreList does not have a next item
+	 * @return next item in scorelist
+	 */
+	public Score next(){
+		if (!hasNext()) {
+			throw new NoSuchElementException();
+		}
+		
+		
+		/* This is what we had before
+		Score result = scorelist.get(currPos);
+		currPos++;
+		return result;
+		*/
+		
+		
+		// This is my change to make sure it returns results with the same category
+		while (hasNext()) {
+			tempScore = scorelist.get(currPos);
+			currPos++;
+			
+			if (tempScore.getCategory().equals(category.substring(0,1))) {
+				break;
+			}
+		}
+		return tempScore;
+	}
 }

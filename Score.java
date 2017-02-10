ecbf23c5af3bf9a54ea7e96e63be1dee1adb7014
Jake VandeWalle <jakevandewalle@gmail.com>
2017-02-10 15:13:37 -0600

    Update Score.java

--- Score.java
+++ Score.java
@@ -1,92 +1,103 @@
-import java.util.NoSuchElementException;
-
 /////////////////////////////////////////////////////////////////////////////
-//Semester:         CS367 Spring 2016 
-//PROJECT:          p1
-//FILE:            ScoreIterator.java
+// Semester:         CS367 Spring 2017
+// PROJECT:          Project 0
+// FILE:             Score.java
 //
-//Authors: (Team 42) Aseel Albeshri, Cory Burich, Margaret Cook, 
-//Jessica Fernandes, Cody Kairis, Jacob Vande Walle
-//Author1: Aseel Albeshri, albeshri@wisc.edu, albeshri,lecture 002
-//Author2: Cory Burich, cburich@wisc.edu, cburich, lecture 002
-//Author3: Margaret Cook, mcook24@wisc.edu, mcook24, lecture 002
-//Author4: Jessica Fernandes, jfernandes2@wisc.edu, jfernandes2, lecture 002
-//Author5: Cody Kairis, kairis@wisc.edu, kairis, lecture 002
-//Author6: Jacob Vande Walle, jvandewalle@wisc.edu, jvandewalle, lecture 002
+// Authors: 		 Jacob Vande Walle
 //
+/////////////////////////////////////////////////////////////////////////////
+
+
 /**
-* 
-* @authors Aseel Albeshri, Cory Burich, Margaret Cook, Jessica Fernandes, 
-* Cody Kairis, Jacob Vande Walle
-/**
- * This class acts as an indirect access iterator. 
- * It implements the ScoreIteratorADT and iterates through the given scores in
- *  ScoreList that match a specified category.
+ * This class is the class for the individual scores. It makes sure that
+ * the parameters passed are valid, otherwise it throws an 
+ * IllegalArgumentException
+ * 
+ * @author Jacob Vande Walle
  *
  */
-public class ScoreIterator implements ScoreIteratorADT<Score> {
-	//Variable of type ScoreList to store the list of scores
-	private ScoreList scoreList;
-	//Variable of type int to store the current position
-	private int currPos;
-	//Variable of type string to store a category
-	private Score tempScore = null;
 
-	/**
-	 * Initializes the instance fields to the passed values 
-	 * and sets the current position to 0.
-	 * PRECONDITIONS: instance fields are null
-	 * POSTCONDITIONS: instance fields are initialized 
-	 * @param scorelist the given list of scores
-	 * @param category the given category
-	 */
-	public ScoreIterator(ScoreList scorelist){
-		this.scoreList = scorelist;
-		currPos = 0;
-	}
-	/**
-	 * Method used to check if scoreList has a next item with the given category
-	 * 
-	 * @return true if scoreList has a Score with the given category, otherwise
-	 * return false
-	 */
+public class Score {
 	
-	public boolean hasNext(String category){
-		for (int i = currPos; currPos < scoreList.size(); i++) {
-
-			if (scoreList.get(i).getCategory().equals(category.substring(0,1))) {
-				return true;
-			}
-		}
-		return false;
-	}
+	String assignmentName;
+	double pointsEarned, pointsPossible;
+	
+	
 	/**
-	 * Method used to get the next item in scoreList 
-	 * @throws NoSuchElementException if scoreList does not have a next item 
-	 * with the given category
-	 * 
-	 * @return next item in scoreList with the given category
+	 * This is the constructor for the score object
+	 * @param name is the name of the assignment
+	 * @param earned is the number of points you earned
+	 * @param possible is the number of points possible
+	 * @throws IllegalArgumentException if the parameters are not within the 
+	 * proper range
 	 */
-	public Score next(String category){
-		if (!hasNext(category)) {
-			throw new NoSuchElementException();
+	public Score(String name, double earned, double possible) throws IllegalArgumentException {
+		if (name == null) {
+			throw new IllegalArgumentException();
+		} else {
+			assignmentName = name;
 		}
 		
-		/* This is what we had before
-		
-		Score result = scorelist.get(currPos);
-		currPos++;
-		return result;
-		*/
-		
-		while (hasNext(category)) {
-			tempScore = scoreList.get(currPos);
-			currPos++;
-			
-			if (tempScore.getCategory().equals(category.substring(0,1))) {
-				break;
-			}
+		if (earned < 0 || earned > possible) {
+			throw new IllegalArgumentException();
+		} else {
+			pointsEarned = earned;
 		}
-		return tempScore;
+		
+		if (possible < 0) {
+			throw new IllegalArgumentException();
+		} else {
+			pointsPossible = possible;
+		}
 	}
+	
+	
+	/**
+	 * Returns the assignment name
+	 * @return assignmentName
+	 */
+	public String getName() {
+		return assignmentName;
+	}
+	
+	
+	/**
+	 * Returns the points earned
+	 * @return pointsEarned
+	 */
+	public double getPoints() {
+		return pointsEarned;
+	}
+	
+	
+	/**
+	 * Returns the maximum points you could have gotten 
+	 * @return pointsPossible
+	 */
+	public double getMaxPossible() {
+		return pointsPossible;
+	}
+	
+	
+	/**
+	 * Returns the first letter of the assignment name
+	 * @return first letter of the assignment name
+	 */
+	public String getCategory() {
+		
+		return assignmentName.substring(0,1);
+	}
+	
+	
+	/**
+	 * 
+	 * @return percentage on the assignment
+	 */
+	public double getPercent() {
+		
+		double percentage = pointsEarned / pointsPossible;
+		percentage = percentage * 100;
+		return percentage;
+	}
+
 }

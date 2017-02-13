398a61e7c15f8c598fb41e4d6c6987b91cf274da
ckairis <kairis@wisc.edu>
2017-02-12 18:44:34 -0600

    Update GradeFileFormatException.java

--- GradeFileFormatException.java
+++ GradeFileFormatException.java
@@ -1,10 +1,16 @@
 /////////////////////////////////////////////////////////////////////////////
 // Semester:         CS367 Spring 2016 
-// PROJECT:          p1
-// FILE:             GradeFileFormatException
+// PROJECT:          program1
+// FILE:             GradeFileFormatException.java
 //
-// Author: Meggie Cook
-// Author1: Meggie Cook, mcook24@wisc.edu, mcook24, Lecture 002
+// Authors: (Team 42) Aseel Albeshri, Cory Burich, Margaret Cook, 
+//          Jessica Fernandes, Cody Kairis, Jacob Vande Walle
+// Author1: Aseel Albeshri, albeshri@wisc.edu, albeshri,lecture 002
+// Author2: Cory Burich, cburich@wisc.edu, cburich, lecture 002
+// Author3: Margaret Cook, mcook24@wisc.edu, mcook24, lecture 002
+// Author4: Jessica Fernandes, jfernandes2@wisc.edu, jfernandes2, lecture 002
+// Author5: Cody Kairis, kairis@wisc.edu, kairis, lecture 002
+// Author6: Jacob Vande Walle, jvandewalle@wisc.edu, jvandewalle, lecture 002
 //
 //////////////////////////// 80 columns wide //////////////////////////////////
 /**
